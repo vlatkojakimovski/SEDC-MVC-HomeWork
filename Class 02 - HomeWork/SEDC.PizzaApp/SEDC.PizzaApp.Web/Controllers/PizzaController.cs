de93e02e15b20cc6a1096939897dc6deb5629048
vlatkojakimovski <jakimovski_vlatko@yahoo.com>
2021-08-16 11:23:47 +0200

    Homework 2 and 3

---
+++ Class 02 - HomeWork/SEDC.PizzaApp/SEDC.PizzaApp.Web/Controllers/PizzaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Web.Controllers
+{
+    //[Route("Pizza")]
+    public class PizzaController : Controller
+    {
+        //[Route("Home")]
+        public IActionResult Index()
+        {
+            List<Pizza> pizzas = StaticDB.Pizzas;
+
+            return new JsonResult(pizzas);
+        }
+
+        //[Route("Details")]
+        public IActionResult Details(int id)
+        {
+            if (StaticDB.Pizzas.Any(x => x.Id == id))
+            {
+                Pizza pizza = StaticDB.Pizzas.SingleOrDefault(x => x.Id == id);
+                return new JsonResult(pizza);
+            } else
+            {
+                //return new EmptyResult();
+
+                return RedirectToAction("Error", "Home");
+            }
+        }
+    }
+}
---
+++ Class 03 - HomeWork/SEDC.PizzaApp/SEDC.PizzaApp.Web/Controllers/PizzaController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Web.Models;
+using SEDC.PizzaApp.Web.Models.Domain;
+using SEDC.PizzaApp.Web.Models.Mapper;
+using SEDC.PizzaApp.Web.Models.ViewModels;
+
+namespace SEDC.PizzaApp.Web.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<PizzaDetailsViewModel> pizzaViewList = new List<PizzaDetailsViewModel>();
+
+            StaticDB.Pizzas.ForEach(x => pizzaViewList.Add(x.PizzaToPizzaViewModel()));
+            
+            ViewData.Add("Title", "Pizza Home Page");
+            ViewData.Add("NumberOfApp", 12);
+
+            var firstPizza = StaticDB.Pizzas.First();
+            //ViewData.Add("Pizza", firstPizza);
+
+            ViewBag.Name = "SEDC Academy";
+            ViewBag.NumberOfPizzas = 2;
+            ViewBag.Pizza = firstPizza;
+
+            return View(pizzaViewList);
+        }
+
+        public IActionResult Details (int id)
+        {
+            if (StaticDB.Pizzas.Any(x => x.Id == id))
+            {
+                Pizza pizza = StaticDB.Pizzas.SingleOrDefault(x => x.Id == id);
+                PizzaDetailsViewModel pizzaViewModel = pizza.PizzaToPizzaViewModel();
+
+                ViewBag.pizzaViewBag = pizzaViewModel;
+                ViewData["Title"] =pizza.Name;
+
+                return View();
+            } else
+            {
+                return new EmptyResult();
+            }
+        }
+    }
+}
